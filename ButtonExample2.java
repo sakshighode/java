14e022d3ab2662b86d656f46c17c94f7f487f73f
sakshighode <sakshighode883@gmail.com>
2025-12-02 14:23:08 +0000

    2/12/2025

---
+++ ButtonExample2.java
@@ -0,0 +1,25 @@
+import javax.swing.*;  
+import java.awt.*;  
+import java.awt.event.*;  
+public class ButtonExample2  
+{  
+Frame fObj;  
+Button button1, button2, button3;   
+ButtonExample2() {  
+fObj = new Frame ("Frame to display buttons");  
+button1 = new Button();  
+button2 = new Button ("Click here");  
+button3 = new Button();  
+button3.setLabel("Button 3");  
+fObj.add(button1);  
+fObj.add(button2);  
+fObj.add(button3);  
+fObj.setLayout(new FlowLayout());  
+fObj.setSize(300,400);  
+fObj.setVisible(true);  
+}   
+public static void main (String args[])  
+{  
+new ButtonExample2();  
+}  
+}  
